0728d387d4e09ad0b5f8817822bcfa2e999d664f
manabshy <manabshy@gmail.com>
2019-01-31 17:03:35 +0000

    ADD getter and setter

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -9,7 +9,6 @@ import { ProductService } from './product.service';
 })
 export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
-    listFilter: string;
     showImage: boolean;
 
     imageWidth: number = 50;
@@ -18,7 +17,15 @@ export class ProductListComponent implements OnInit {
 
     filteredProducts: IProduct[];
     products: IProduct[];
+    private _listFilter: string;
 
+    get listFilter (): string {
+        return this._listFilter;
+    }
+    set listFilter (value: string) {
+        this._listFilter = value;
+        this.performFilter(this._listFilter);
+    }
     constructor(private productService: ProductService) { }
 
     ngOnInit(): void {
