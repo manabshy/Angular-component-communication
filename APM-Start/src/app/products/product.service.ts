1632cb3f2c1d5069a48007408d6b95b9dc92ebd0
manabshy <manabshy@gmail.com>
2019-02-18 22:33:32 +0000

    completing the state management service

--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -12,13 +12,17 @@ import { IProduct } from './product';
 @Injectable()
 export class ProductService {
     private productsUrl = 'api/products';
-
+    private products: IProduct[];
     constructor(private http: HttpClient) { }
 
     getProducts(): Observable<IProduct[]> {
+        if (this.products) {
+            return of(this.products);
+        }
         return this.http.get<IProduct[]>(this.productsUrl)
                         .pipe(
-                            tap(data => console.log(JSON.stringify(data))),
+                            tap(data => console.log('Products Data:', JSON.stringify(data))),
+                            tap(data => this.products = data),
                             catchError(this.handleError)
                         );
     }
@@ -27,12 +31,19 @@ export class ProductService {
         if (id === 0) {
             return of(this.initializeProduct());
         }
+        if (this.products) {
+            const foundItem = this.products.find(item => item.id === id);
+            if (foundItem) {
+                return of(foundItem);
+            }
+        }
         const url = `${this.productsUrl}/${id}`;
         return this.http.get<IProduct>(url)
                         .pipe(
-                            tap(data => console.log('Data: ' + JSON.stringify(data))),
+                            tap(data => console.log('Data reretrieved: ' + JSON.stringify(data))),
                             catchError(this.handleError)
                         );
+
     }
 
     saveProduct(product: IProduct): Observable<IProduct> {
@@ -50,6 +61,13 @@ export class ProductService {
         return this.http.delete<IProduct>(url, { headers: headers} )
                         .pipe(
                             tap(data => console.log('deleteProduct: ' + id)),
+                            tap(data => {
+                                // Find the item index and delete it from the products array
+                                const foundItemIdx = this.products.findIndex(item => item.id === id);
+                                if (foundItemIdx > -1) {
+                                    this.products.splice(foundItemIdx, 1);
+                                }
+                            }),
                             catchError(this.handleError)
                         );
     }
@@ -59,6 +77,7 @@ export class ProductService {
         return this.http.post<IProduct>(this.productsUrl, product,  { headers: headers} )
                         .pipe(
                             tap(data => console.log('createProduct: ' + JSON.stringify(data))),
+                            tap(data => this.products.push(data)),
                             catchError(this.handleError)
                         );
     }
